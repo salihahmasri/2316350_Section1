a121170df84382ddb7e80110b2fc916c96c4746d
salihahmasri <165368152+salihahmasri@users.noreply.github.com>
2024-06-26 10:43:03 +0800

    education

---
+++ education.js
@@ -0,0 +1,35 @@
+// Every have the same function. It used to hide and show when the user click the button
+//I have many funtion since i use different name for id 
+
+function myFunction() {
+    var x = document.getElementById("school1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function myFunction1() {
+    var x = document.getElementById("school2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function myFunction2() {
+    var x = document.getElementById("foundation");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function myFunction3() {
+    var x = document.getElementById("now");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
